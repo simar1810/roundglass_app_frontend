a2881fd63594947a8957e60a41735447e7e50ae0
dnyaneshwar411 <dnyaneshwarpk20@gmail.com>
2025-04-11 14:41:20 +0530

    resume

--- src/app/(authorized)/coach/tools/feed/page.jsx
+++ src/app/(authorized)/coach/tools/feed/page.jsx
@@ -1,5 +1,6 @@
+import NoData from "@/components/common/NoData";
 import { Button } from "@/components/ui/button";
-import { Globe, Users } from "lucide-react";
+import { Bookmark, Globe, Images, Plus, Users } from "lucide-react";
 
 export default function Page() {
   return <div className="mt-8">
@@ -19,8 +20,26 @@ export default function Page() {
       <Button
         className="text-[12px] font-bold bg-[var(--accent-1)] text-white hover:bg-[var(--accent-1)] hover:text-white ml-auto rounded-[16px]"
       >
-        + New Post
+        <Plus />
+        New Post
       </Button>
+    </div>
+
+    <div className="max-w-[650px] mt-10 mx-auto relative">
+      <div className="sticky top-0 rounded-t-[10px] border-1 border-[var(--dark-1)]/10 overflow-clip">
+        <Button className="w-1/2 text-center text-[var(--accent-1)] text-[12px] bg-transparent hover:bg-[var(--comp-1)] border-r-2 shadow-none rounded-none">
+          <Images />
+          My Posts
+        </Button>
+        <Button className="w-1/2 text-center text-[var(--dark-2)] text-[12px] bg-transparent hover:bg-[var(--comp-1)] border-r-2 shadow-none rounded-none">
+          <Bookmark />
+          My Posts
+        </Button>
+      </div>
+    </div>
+
+    <div className="min-h-[400px] flex items-center">
+      <NoData message="No Posts Available" />
     </div>
   </div>
 }
