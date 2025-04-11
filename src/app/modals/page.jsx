81539cc11e8ed03faa4150b7136b130fc3490d13
Ayush Thukral <krishayush45@gmail.com>
2025-04-11 14:38:07 +0530

    modals

--- src/app/modals/page.jsx
+++ src/app/modals/page.jsx
@@ -1,7 +1,14 @@
+import AddClientDetailsModal from "@/components/modals/AddClientDetailsModal";
 import AddClientModal from "@/components/modals/AddClientModal";
+import ClientCreatedNotiModal from "@/components/modals/ClientCreatedNotiModals";
+
+import ProfileModal from "@/components/modals/ProfileModal";
 
 export default function Page() {
   return <div className="p-20 flex items-center gap-2">
-    <AddClientModal />
+    <AddClientModal/>
+    <ClientCreatedNotiModal/>
+    <ProfileModal/>
+    <AddClientDetailsModal/>
   </div>
 }
