14397d1b267406544cfe8b3bf9ce0a816191f65a
Ayush Thukral <krishayush45@gmail.com>
2025-05-03 00:45:17 +0530

    modals

--- src/app/modals/page.jsx
+++ src/app/modals/page.jsx
@@ -5,38 +5,53 @@ import AddRecipeModal from "@/components/modals/AddRecipeModal";
 import AssignMealModal from "@/components/modals/Assignmealmodal";
 import AssignWorkoutModal from "@/components/modals/AssignModal";
 import AssignWorkoutAddModal from "@/components/modals/AssignModeladd";
+import AssignNewAppointmentModal from "@/components/modals/AssignNewAppointmentModal";
+import BrandingModal from "@/components/modals/BrandingModal";
 import ClientCreatedNotiModal from "@/components/modals/ClientCreatedNotiModals";
 import AddSelectClientModal from "@/components/modals/clientmodal";
 import CreateMealModal from "@/components/modals/CreateMealModal";
+import NewAppointmentModal from "@/components/modals/NewAppointmentModal";
+import NewNotesModal from "@/components/modals/NewNotesAdd";
 import NewRecipeModal from "@/components/modals/NewRecipeModal";
 import OrderSuccessModal from "@/components/modals/ordersuccessmodal";
+import ProgramModal from "@/components/modals/PorgramModal";
 import AddPostModal from "@/components/modals/postmodal";
 import AddPostModalVideo from "@/components/modals/postmodalvideo";
 
 import ProfileModal from "@/components/modals/ProfileModal";
 import RecipieSuccessModal from "@/components/modals/recipiesuccessfullmodel";
 import SelectClientModal from "@/components/modals/SelectClientModal";
+import SelectLanguageModal from "@/components/modals/SelectLanguageModal";
 import ShoppingCartModal from "@/components/modals/shoppingcartmodel";
 
 export default function Page() {
-  return <div className="p-20 flex items-center gap-2">
-    <AddClientModal/>
-    <ClientCreatedNotiModal/>
-    <ProfileModal/>
-    <AddClientDetailsModal/>
-    <AddPostModal/>
-    <AddPostModalVideo/>
-    <SelectClientModal/>
-    <ShoppingCartModal/>
-    <OrderSuccessModal/>
-    <AddSelectClientModal/>
-    <AssignWorkoutModal/>
-    <AssignWorkoutAddModal/>
-    <AssignMealModal/>
-    <RecipieSuccessModal/>
-    <NewRecipeModal/>
-    <AddPlanModal/>
-    <CreateMealModal/>
-    <AddRecipeModal/>
-  </div>
+  return (
+    <div className="p-20 flex items-center gap-2">
+      <AddClientModal />
+      <ClientCreatedNotiModal />
+      <ProfileModal />
+      <AddClientDetailsModal />
+      <AddPostModal />
+      <AddPostModalVideo />
+      <SelectClientModal />
+      <ShoppingCartModal />
+      <OrderSuccessModal />
+      <AddSelectClientModal />
+      <AssignWorkoutModal />
+      <AssignWorkoutAddModal />
+      <AssignMealModal />
+      <RecipieSuccessModal />
+      <NewRecipeModal />
+      <AddPlanModal />
+      <CreateMealModal />
+      <AddRecipeModal />
+      <NewNotesModal />
+      <NewAppointmentModal />
+      <AssignNewAppointmentModal />
+      <ProgramModal/>
+      <SelectLanguageModal/>
+      <BrandingModal/>
+      
+    </div>
+  );
 }
--- src/components/common/Guardian.jsx
+++ src/components/common/Guardian.jsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation"
 export default function Guardian({ children }) {
   const { coach, isLoggedIn } = useAppSelector(state => state.coach)
 
-  if (!coach || !isLoggedIn) redirect("/login");
+  if (coach || isLoggedIn) redirect("/login");
 
   return { children }
 }
