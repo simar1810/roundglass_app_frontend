6f655d6a73610186881f3f309e84d1b0b38f47d6
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2025-07-29 11:24:27 +0530

    fix: minor changes

--- src/app/(authorized)/coach/(pro-plan-features)/workouts/add/[mode]/page.jsx
+++ src/app/(authorized)/coach/(pro-plan-features)/workouts/add/[mode]/page.jsx
@@ -2,10 +2,9 @@
 import ContentError from "@/components/common/ContentError";
 import Stage2 from "@/components/pages/coach/workouts/add/Stage2";
 import { customWorkoutIS, customWorkoutReducer, selectWorkoutType } from "@/config/state-reducers/custom-workout";
-import useBlockNavigation from "@/hooks/useBlockNavigation";
 import useCurrentStateContext, { CurrentStateProvider } from "@/providers/CurrentStateContext";
-import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
   return <CurrentStateProvider
@@ -19,12 +18,6 @@ export default function Page() {
 function CustomMealPlanContainer() {
   const { mode } = useParams()
   const { dispatch } = useCurrentStateContext()
-  const [dataSavedLocally, setDataSavedLocally] = useState(false)
-
-  useBlockNavigation({
-    shouldBlock: () => true,
-    onAttemptToLeave: (e) => setDataSavedLocally(true)
-  })
 
   useEffect(function () {
     if (["daily", "weekly", "monthly"].includes(mode)) {
--- src/app/(authorized)/coach/(pro-plan-features)/workouts/add/page.jsx
+++ src/app/(authorized)/coach/(pro-plan-features)/workouts/add/page.jsx
@@ -30,12 +30,6 @@ function CustomWorkoutContainer() {
   const { dispatch, stage } = useCurrentStateContext();
   const Component = selectCreationStage(stage);
 
-  function handleBeforeUnload(event) {
-    console.log("User is leaving the page");
-    event.preventDefault();
-    event.returnValue = "";
-  };
-
   useEffect(function () {
     ; (async function () {
       if (["edit", "copy_edit"].includes(creationType) && Boolean(workoutId)) {
@@ -63,12 +57,6 @@ function CustomWorkoutContainer() {
         dispatch(selectWorkoutType(mode));
       }
     })();
-  }, [])
-
-  useEffect(function () {
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => window.removeEventListener("beforeunload", handleBeforeUnload)
   }, [])
 
   return Component;
