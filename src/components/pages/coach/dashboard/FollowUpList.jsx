5533d62530c6eb6e15616ed3bf924a0b1614b415
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2025-08-18 13:29:12 +0530

    fix: minor changes

--- src/components/pages/coach/dashboard/FollowUpList.jsx
+++ src/components/pages/coach/dashboard/FollowUpList.jsx
@@ -1,10 +1,16 @@
+import { parse } from "date-fns";
 import ClientListFollowUp from "./ClientListFollowUp";
 
 export default function FollowUpList({ clients }) {
+  const sortedClients = clients.sort((a, b) => {
+    const dateA = parse(a.nextFollowup, "dd-MM-yyyy", new Date())
+    const dateB = parse(b.nextFollowup, "dd-MM-yyyy", new Date())
+    return dateA - dateB
+  })
   return <div className="bg-white py-4 rounded-[10px] border-1">
     <div className="text-[14px] font-bold mb-4 px-4">Pending Followups</div>
     <div className="divide-y-1 divide-[#ECECEC]">
-      {clients.map(client => <ClientListFollowUp
+      {sortedClients.map(client => <ClientListFollowUp
         key={client.clientId}
         src={client.profilePhoto}
         name={client.name}
