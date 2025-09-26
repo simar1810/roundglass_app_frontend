3adc1e72fc0016146039c4067c5a1607faa3ef89
Hardik Agarwal <hardikapj@gmail.com>
2025-09-26 21:52:40 +0530

    added onboarding coaches and roles in downline

--- src/components/pages/coach/downline/Visualizer.jsx
+++ src/components/pages/coach/downline/Visualizer.jsx
@@ -5,6 +5,21 @@ import { Network } from "vis-network/standalone";
 import { DataSet } from "vis-data/peer";
 import { toast } from "sonner";
 import Cookies from "js-cookie";
+import { nameInitials } from "@/lib/formatter";
+
+const colorFromString = (str) => {
+	let hash = 0;
+	if (!str || str.length === 0) return "#cccccc";
+	for (let i = 0; i < str.length; i++) {
+		hash = str.charCodeAt(i) + ((hash << 5) - hash);
+	}
+	let color = "#";
+	for (let i = 0; i < 3; i++) {
+		let value = (hash >> (i * 8)) & 0xff;
+		color += ("00" + value.toString(16)).substr(-2);
+	}
+	return color;
+};
 
 const TreeVisualizer = ({ initialNode }) => {
 	const visJsRef = useRef(null);
@@ -13,9 +28,7 @@ const TreeVisualizer = ({ initialNode }) => {
 	const [expandedNodes, setExpandedNodes] = useState(new Set());
 
 	const expandNode = async (nodeId) => {
-		if (!nodeId || expandedNodes.has(nodeId)) {
-			return;
-		}
+		if (!nodeId || expandedNodes.has(nodeId)) return;
 
 		try {
 			const token = Cookies.get("token");
@@ -34,51 +47,35 @@ const TreeVisualizer = ({ initialNode }) => {
 
 			if (body.status_code === 200 && body.data) {
 				const { nodes: newNodes, edges: newEdges } = body.data;
+
 				if (newNodes && newNodes.length > 0) {
 					const nodesWithDetails = newNodes.map((node) => {
 						const tooltipElement = document.createElement("div");
 						tooltipElement.innerHTML = `
                             <div style="padding: 5px; color: #333;">
-                                <p style="margin: 0;"><b>Name:</b> ${
-																	node.label
-																}</p>
-                                <p style="margin: 0;"><b>ID:</b> ${
-																	node.coachId
-																}</p>
-                                <p style="margin: 0;"><b>Email:</b> ${
-																	node.email
-																}</p>
-                                <p style="margin: 0;"><b>Clients:</b> ${
-																	node.totalClients || 0
-																}</p>
+                                <p style="margin: 0;"><b>Name:</b> ${node.label}</p>
+                                <p style="margin: 0;"><b>Category:</b> ${node.categoryName}</p>
+                                <p style="margin: 0;"><b>ID:</b> ${node.coachId}</p>
                             </div>
                         `;
-						function calcInit(node) {
-							if (node.label.split(" ").length == 1) {
-								return node.label.split(" ")[0][0];
-							} else {
-								return (
-									node.label.split(" ")[0][0] +
-									" " +
-									node.label.split(" ")[1][0]
-								);
-							}
-						}
 						return {
 							...node,
+							label: nameInitials(node.label),
 							title: tooltipElement,
-							label: calcInit(node),
+							color: {
+								border: colorFromString(node.categoryName),
+								background: "#3c3c3c",
+							},
 						};
 					});
+
 					const edgesWithIds = newEdges.map((edge) => ({
 						...edge,
 						id: `${edge.from}-${edge.to}`,
 					}));
-
 					nodes.update(nodesWithDetails);
 					edges.update(edgesWithIds);
 				}
-
 				setExpandedNodes((prev) => new Set(prev).add(nodeId));
 			}
 		} catch (error) {
@@ -89,28 +86,14 @@ const TreeVisualizer = ({ initialNode }) => {
 
 	useEffect(() => {
 		const data = { nodes, edges };
-
 		const options = {
-			layout: {
-				hierarchical: {
-					direction: "DU",
-					sortMethod: "directed",
-					levelSeparation: 100,
-					nodeSpacing: 150,
-				},
-			},
-
-			physics: {
-				enabled: false,
-			},
+			physics: { enabled: true },
 			nodes: {
 				shape: "circle",
 				size: 25,
 				font: { size: 16, color: "#e0e0e0", face: "Arial" },
-				borderWidth: 2,
+				borderWidth: 3,
 				color: {
-					border: "#00aeff",
-					background: "#3c3c3c",
 					highlight: { border: "#ffffff", background: "#555555" },
 					hover: { border: "#ffffff", background: "#444444" },
 				},
@@ -131,20 +114,21 @@ const TreeVisualizer = ({ initialNode }) => {
 			visJsRef.current && new Network(visJsRef.current, data, options);
 
 		network?.on("click", (params) => {
-			if (params.nodes.length > 0) {
-				const nodeId = params.nodes[0];
-				expandNode(nodeId);
-			}
+			if (params.nodes.length > 0) expandNode(params.nodes[0]);
 		});
-		console.log(initialNode);
+
 		if (initialNode && nodes.length === 0) {
 			const initialTooltipElement = document.createElement("div");
 			initialTooltipElement.innerHTML = initialNode.title;
-			console.log(initialTooltipElement);
-			nodes.add({
+			const nodeToAdd = {
 				...initialNode,
 				title: initialTooltipElement,
-			});
+				color: {
+					...initialNode.color,
+					border: colorFromString(initialNode.categoryName),
+				},
+			};
+			nodes.add(nodeToAdd);
 		}
 
 		return () => {
