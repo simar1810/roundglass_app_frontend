d1e9d6f2150912b8cbde242632460c7fb51ff03a
Dnyaneshwar <dnyaneshwarpk20@gmail.com>
2025-09-29 11:01:41 +0530

    fix: visualizer fixe

--- src/components/pages/coach/downline/Visualizer.jsx
+++ src/components/pages/coach/downline/Visualizer.jsx
@@ -125,7 +125,7 @@ const TreeVisualizer = ({ initialNode }) => {
 					border: colorFromString(initialNode.categoryName),
 				},
 			});
-			nodes.add(nodeToAdd);
+			// nodes.add(nodeToAdd);
 		}
 
 		return () => {
